d04f903dbc2841d325c9e290b04fcb7dd528f0c2
Bianca <ing_biank@hotmail.com>
2019-05-01 11:54:37 +0200

    adds FoodBox component funtionality and displays in App.js

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import './App.css'
-import JSONfoot from './foods.json'
+import JSONfood from './foods.json'
+import FoodBox from './components/FoodBox'
 
 class App extends Component {
+    state = { food: JSONfood.slice() }
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                <FoodBox food={this.state.food} />
             </div>
         )
     }
